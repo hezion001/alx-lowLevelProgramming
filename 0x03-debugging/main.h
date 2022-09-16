33da6aa3d3b721c659124fd2b8c016626b61c8a0
Hezekiah Ibok <hezekiahibok@gmail.com>
2022-09-16 22:24:04 +0100

    Task modified

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,6 @@
-#ifdef HEADER_F
-#define HEADER_F
+#ifdef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
